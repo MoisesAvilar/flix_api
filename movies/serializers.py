66d6048b13353255cf2f16f887d4b782f4749a50
Moisés <moisesavilar0@gmail.com>
2024-02-14 20:24:47 -0300

    API finalizada

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,9 +1,30 @@
+from django.db.models import Avg
 from rest_framework import serializers
 
 from movies.models import Movie
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return rate
+
+        return None
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('O ano deve ser superior a 1990.')
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Limitado a apenas 200 caracteres')
+        return value
--- movies/views.py
+++ movies/views.py
@@ -1,14 +1,18 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
+from app.permissions import GlobalDefaultPermission
 from movies.models import Movie
-from movies.serializers import MovieSerializer
+from movies.serializers import MovieModelSerializer
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieModelSerializer
 
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieModelSerializer
